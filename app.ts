6c1f61920a8fc279d0f2f12db7f601aaa4a60203
Adrian Pop <adrian.pop2@student.upt.ro>
2023-12-05 23:26:32 +0200

    Added configuration file + gitignore

--- app.ts
+++ app.ts
@@ -1,50 +1,72 @@
 import { request, gql } from 'graphql-request';
-import { promises as fs } from 'fs';
+import * as fs from 'fs/promises';
+import * as yaml from 'js-yaml';
 
-const gitlabApiUrl = 'https://gitlab.com/api/graphql';
-const personalAccessToken = ''; //add token
+interface AppConfig {
+  gitlabApiUrl: string;
+  projectFullPath: string;
+  tokens: string[];
+} 
 
-const headers = {
-  Authorization: `Bearer ${personalAccessToken}`,
-};
-
-const queries: string[] = [];
-
-const newQuery = gql`
-  {
-    project(fullPath: "octopus-code/octopus") {
-      name
-      issues {
-        nodes {
-          title
-        }
-      }
-      mergeRequests {
-        nodes {
-          title
-          author {
-            name
-          }
-          approved
-        }
-      }
-    }
-  }
-`;
-
-queries.push(newQuery);
+const configFile = 'config.yml';
 
 (async () => {
-  for (let index = 0; index < queries.length; index++) {
-    const query = queries[index];
-    try {
-      const result = await request(gitlabApiUrl, query, {}, headers);
-      const filename = `result_${index + 1}.json`;
+  try {
+    const configYaml = await fs.readFile(configFile, 'utf8');
+    const config: AppConfig = yaml.load(configYaml) as AppConfig;
 
-      await fs.writeFile(filename, JSON.stringify(result, null, 2));
-      console.log(`Query ${index + 1} result saved to ${filename}`);
-    } catch (error) {
-      console.error(`Error executing query ${index + 1}:`, error);
-    }
+    const gitlabApiUrl = config.gitlabApiUrl;
+    const projectFullPath = config.projectFullPath;
+    const tokens = config.tokens;
+
+    const queries: string[] = [];
+
+    const newQuery = gql`
+      {
+        project(fullPath: "${projectFullPath}") {
+          name
+          issues {
+            nodes {
+              title
+            }
+          }
+          mergeRequests {
+            nodes {
+              title
+              author {
+                name
+              }
+              approved
+            }
+          }
+        }
+      }
+    `;
+
+    queries.push(newQuery);
+
+    (async () => {
+      for (let tokenIndex = 0; tokenIndex < tokens.length; tokenIndex++) {
+        const token = tokens[tokenIndex];
+
+        for (let index = 0; index < queries.length; index++) {
+            const query = queries[index];
+
+            try {
+                const result = await request(gitlabApiUrl, query, {}, { Authorization: `Bearer ${token}` });
+                const filename = `result_${tokenIndex + 1}_${index + 1}.json`;
+
+                await fs.writeFile(filename, JSON.stringify(result, null, 2));
+                console.log(`Query ${index + 1} result saved to ${filename}`);
+
+            } catch (error) {
+                console.error(`Error executing query ${index + 1}:`, error);
+            }
+        }
+      }
+    }) ();
+
+} catch (error) {
+    console.error('Error reading or parsing the configuration file:', error);
   }
-})();
+}) ();
