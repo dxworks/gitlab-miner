fec33666b10789851026db4d883480ff8183e6b9
Adrian Pop <adrian.pop2@student.upt.ro>
2023-12-06 20:01:18 +0200

    Check functionality.

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ const configFile = 'config.yml';
 
     const queries: string[] = [];
 
-    const newQuery = gql`
+    const Query_1 = gql`
       {
         project(fullPath: "${projectFullPath}") {
           name
@@ -43,28 +43,51 @@ const configFile = 'config.yml';
       }
     `;
 
-    queries.push(newQuery);
-
-    (async () => {
-      for (let tokenIndex = 0; tokenIndex < tokens.length; tokenIndex++) {
-        const token = tokens[tokenIndex];
-
-        for (let index = 0; index < queries.length; index++) {
-            const query = queries[index];
-
-            try {
-                const result = await request(gitlabApiUrl, query, {}, { Authorization: `Bearer ${token}` });
-                const filename = `result_${tokenIndex + 1}_${index + 1}.json`;
-
-                await fs.writeFile(filename, JSON.stringify(result, null, 2));
-                console.log(`Query ${index + 1} result saved to ${filename}`);
-
-            } catch (error) {
-                console.error(`Error executing query ${index + 1}:`, error);
+    const Query_2 = gql`
+    {
+      project(fullPath: "${projectFullPath}") {
+        name
+        issues {
+          nodes {
+            title
+          }
+        }
+        mergeRequests {
+          nodes {
+            title
+            author {
+              name
             }
+            approved
+          }
         }
       }
-    }) ();
+    }
+  `;
+
+  queries.push(Query_1);
+  queries.push(Query_2);
+
+  (async () => {
+    for (let tokenIndex = 0; tokenIndex < tokens.length; tokenIndex++) {
+      const token = tokens[tokenIndex];
+
+      for (let queryIndex = 0; queryIndex < queries.length; queryIndex++) {
+          const query = queries[queryIndex];
+
+          try {
+              const result = await request(gitlabApiUrl, query, {}, { Authorization: `Bearer ${token}` });
+              const filename = `result_${tokenIndex + 1}_${queryIndex + 1}.json`;
+
+              await fs.writeFile(filename, JSON.stringify(result, null, 2));
+              console.log(`Query ${queryIndex + 1} result saved to ${filename}`);
+
+          } catch (error) {
+              console.error(`Error executing query ${queryIndex + 1}:`, error);
+          }
+      }
+    }
+  }) ();
 
 } catch (error) {
     console.error('Error reading or parsing the configuration file:', error);
